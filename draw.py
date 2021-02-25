60a74bdf8e7468cb5a44cd5c2baf26fcbf3d028d
Grace Cantarella <gracecant888@gmail.com>
2021-02-24 22:35:16 -0500

    finished line algorithm and final image

--- draw.py
+++ draw.py
@@ -3,73 +3,78 @@ from display import *
 #bresenham's line algorithm
 def draw_line( x0, y0, x1, y1, screen, color ):
 
-    #check if x0 is smaller than x1. If not, switch
+    #check if x0 is smaller than x1. if not, switch
     if (x0 > x1):
         x0, x1 = x1, x0
         y0, y1 = y1, y0
 
-    #variables
-    x0, y0 = int(x0), int(y0)
+    #set initial variables
+    x = x0
+    y = y0
+
+    x, y = int(x), int(y)
     x1, y1 = int(x1), int(y1)
 
-    #other variables
-    A = y1 - y0
-    B = -(x1 - x0)
-    if B!=0:
-        m = A / B
+    dx = x1-x
+    dy = y1-y
+    if dx != 0:
+        m = dy/dx
+
+    A = dy
+    B = -dx
 
     #undefined slope (vertical line)
     if B == 0:
-        while (y0 <= y1):
-            plot(screen, color, x0, y0)
-            y0+=1
+        while (y <= y1):
+            plot(screen, color, x, y)
+            y+=1
 
     #zero slope (horizontal line)
     elif A == 0:
-        while (x0 <= x1):
-            plot(screen, color, x0, y0)
-            x0+=1
+        while (x <= x1):
+            plot(screen, color, x, y)
+            x+=1
 
     #octant 1 and 5
     elif m <= 1 and m > 0:
         d = 2*A + B
-        while (x0 <= x1):
-            plot(screen, color, x0, y0)
+        while (x <= x1):
+            plot(screen, color, x, y)
             if d > 0:
+                y+=1
                 d+=(2*B)
-                y0+=1
-            x0+=1
+            x+=1
             d+=(2*A)
 
     #octant 2 and 6
     elif m > 1:
         d = 2*B + A
-        while (y0 <= y1):
-            plot(screen, color, x0, y0)
+        while (y <= y1):
+            plot(screen, color, x, y)
             if d < 0:
                 d+=(2*A)
-                x0+=1
-            y0+=1
+                x+=1
+            y+=1
             d+=(2*B)
 
     #octant 3 and 7
     elif m < -1:
         d = A - 2*B
-        while (y0 <= y1):
-            plot(screen, color, x0, y0)
+        while (y >= y1):
+            plot(screen, color, x, y)
             if d > 0:
-                d+=(2*B)
-                y0+=1
-            x0-=1
+                d+=(2*A)
+                x+=1
+            y-=1
             d-=(2*B)
 
     #octant 4 and 8
     elif m >= -1 and m < 0:
         d = 2*A - B
-        while (x0 <= x1):
-            plot(screen, color, x0, y0)
+        while (x <= x1):
+            plot(screen, color, x, y)
             if d < 0:
-                d+=(2*B)
-                y0+=1
-            x0+=1
+                d-=(2*B)
+                y-=1
+            x+=1
             d+=(2*A)
--- main.py
+++ main.py
@@ -1,40 +1,73 @@
 from display import *
 from draw import *
+import random
 
 s = new_screen()
-c = [ 0, 255, 0 ]
+c = [0, 255, 0]
+
+#sun
+for i in range(100):
+     c[RED]=random.randint(220,250)
+     c[GREEN]=random.randint(150,250)
+     c[BLUE]=random.randint(0,30)
+     draw_line(490, 420, 390+random.randint(-170, 200), 390+random.randint(-150, 200), s, c)
+
+#river: dark
+c = [30, 141, 250]
+for i in range(32):
+    draw_line(0, 160-(i*5), XRES, 160-(i*5), s, c)
+
+#river: light
+c = [100, 149, 237]
+for i in range(8):
+    draw_line(125, 157-(i*20), 375, 157-(i*20), s, c)
+for i in range(16):
+    draw_line(100, 158-(i*10), 400, 158-(i*10), s, c)
+for i in range(8):
+    draw_line(50, 158-(i*20), 450, 158-(i*20), s, c)
+
+#river: white
+c = [173, 215, 230]
+for i in range(4):
+    draw_line(200, 157-(i*40), 300, 157-(i*40), s, c)
+for i in range(4):
+    draw_line(150, 158-(i*40), 350, 158-(i*40), s, c)
+for i in range(8):
+    draw_line(225, 158-(i*20), 275, 158-(i*20), s, c)
 
 '''
-#octants 1 and 5
+c = [ 0, 255, 0 ]
+
+#octants 1 and 5, works!
 draw_line(0, 0, XRES-1, YRES-1, s, c)
 draw_line(0, 0, XRES-1, YRES / 2, s, c)
 draw_line(XRES-1, YRES-1, 0, YRES / 2, s, c)
 
-#octants 8 and 4
+#octants 8 and 4, works!
 c[BLUE] = 255;
 draw_line(0, YRES-1, XRES-1, 0, s, c);
 draw_line(0, YRES-1, XRES-1, YRES/2, s, c);
 draw_line(XRES-1, 0, 0, YRES/2, s, c);
 
-#octants 2 and 6
+#octants 2 and 6, works!
 c[RED] = 255;
 c[GREEN] = 0;
 c[BLUE] = 0;
 draw_line(0, 0, XRES/2, YRES-1, s, c);
 draw_line(XRES-1, YRES-1, XRES/2, 0, s, c);
 
-#octants 7 and 3
+#octants 7 and 3, works!
 c[BLUE] = 255;
 draw_line(0, YRES-1, XRES/2, 0, s, c);
 draw_line(XRES-1, 0, XRES/2, YRES-1, s, c);
 
-'''
-#horizontal and vertical
+
+#horizontal and vertical, works!
 c[BLUE] = 0;
 c[GREEN] = 255;
 draw_line(0, YRES/2, XRES-1, YRES/2, s, c);
 draw_line(XRES/2, 0, XRES/2, YRES-1, s, c);
-
+'''
 
 display(s)
 save_ppm(s, 'binary.ppm')
